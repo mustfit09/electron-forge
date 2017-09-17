6c82a0b25e8598cc6e4cdc4dc578e486dfa7b549
Mark Lee <electronjs@lazymalevolence.com>
2017-09-17 16:51:54 -0700

    refactor(maker): replace zip-folder with cross-zip
    ISSUES CLOSED: #322

--- packages/maker/zip/src/MakerZIP.js
+++ packages/maker/zip/src/MakerZIP.js
@@ -1,8 +1,8 @@
 import MakerBase from '@electron-forge/maker-base';
 
-import { spawn } from 'child_process';
 import path from 'path';
 import pify from 'pify';
+import { zip } from 'cross-zip';
 
 export default class MakerZIP extends MakerBase {
   name = 'zip';
@@ -18,39 +18,13 @@ export default class MakerZIP extends MakerBase {
     packageJSON,
     targetPlatform,
   }) {
-    const zipFolder = require('zip-folder');
+    const zipDir = ['darwin', 'mas'].includes(targetPlatform) ? path.resolve(dir, `${appName}.app`) : dir;
 
     const zipPath = path.resolve(makeDir, `${path.basename(dir)}-${packageJSON.version}.zip`);
+
     await this.ensureFile(zipPath);
-    switch (targetPlatform) {
-      case 'win32':
-        await pify(zipFolder)(dir, zipPath);
-        break;
-      case 'mas':
-      case 'darwin':
-        await this.zipPromise(path.resolve(dir, `${appName}.app`), zipPath);
-        break;
-      case 'linux':
-        await this.zipPromise(dir, zipPath);
-        break;
-      default:
-        throw `Unrecognized platform: ${process.platform}`;
-    }
+    await pify(zip)(zipDir, zipPath);
+
     return [zipPath];
   }
-
-  zipPromise = (from, to) =>
-    new Promise((resolve, reject) => {
-      const child = spawn('zip', ['-r', '-y', to, path.basename(from)], {
-        cwd: path.dirname(from),
-      });
-
-      child.stdout.on('data', () => {});
-      child.stderr.on('data', () => {});
-
-      child.on('close', (code) => {
-        if (code === 0) return resolve();
-        reject(new Error(`Failed to zip, exitted with code: ${code}`));
-      });
-    });
 }
